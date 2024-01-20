d15feee2228e5f65d1b7b6b1df5766213735d188
Bohdan <b.yaronnyi@gmail.com>
2024-01-20 17:05:03 +0200

    first commit

---
+++ src/Resources/public/js/main.js
@@ -0,0 +1,9 @@
+$('.slider__gallery').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: false,
+    arrows: true,
+    prevArrow:"<img class='slick-arrow slick-prev-c' src='/src/Resources/public/img/slider-img/slider-arrow-left.svg'>",
+    nextArrow:"<img class='slick-arrow slick-next-c' src='/src/Resources/public/img/slider-img/slider-arrow-right.svg'>"
+  });
