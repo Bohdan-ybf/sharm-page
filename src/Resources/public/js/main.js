ba4249a615ff2c6706fcfb2924115031ffeb9318
Bohdan <b.yaronnyi@gmail.com>
2024-01-20 23:40:21 +0200

    updated

--- src/Resources/public/js/main.js
+++ src/Resources/public/js/main.js
@@ -5,5 +5,21 @@ $('.slider__gallery').slick({
     infinite: false,
     arrows: true,
     prevArrow:"<img class='slick-arrow slick-prev-c' src='./src/Resources/public/img/slider-img/slider-arrow-left.svg'>",
-    nextArrow:"<img class='slick-arrow slick-next-c' src='./src/Resources/public/img/slider-img/slider-arrow-right.svg'>"
+    nextArrow:"<img class='slick-arrow slick-next-c' src='./src/Resources/public/img/slider-img/slider-arrow-right.svg'>",
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
