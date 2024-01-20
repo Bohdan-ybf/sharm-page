a81ae13cccae60505e9169e7528b27192d5bfe0d
Bohdan <b.yaronnyi@gmail.com>
2024-01-20 17:14:56 +0200

    updated

--- src/Resources/public/js/main.js
+++ src/Resources/public/js/main.js
@@ -4,6 +4,6 @@ $('.slider__gallery').slick({
     slidesToScroll: 1,
     infinite: false,
     arrows: true,
-    prevArrow:"<img class='slick-arrow slick-prev-c' src='/src/Resources/public/img/slider-img/slider-arrow-left.svg'>",
-    nextArrow:"<img class='slick-arrow slick-next-c' src='/src/Resources/public/img/slider-img/slider-arrow-right.svg'>"
+    prevArrow:"<img class='slick-arrow slick-prev-c' src='./src/Resources/public/img/slider-img/slider-arrow-left.svg'>",
+    nextArrow:"<img class='slick-arrow slick-next-c' src='./src/Resources/public/img/slider-img/slider-arrow-right.svg'>"
   });
