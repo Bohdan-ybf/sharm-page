9d2db31f0e47953e3b9b5be6935e50a335db9b34
Bohdan <b.yaronnyi@gmail.com>
2024-01-22 09:37:14 +0200

    updated

--- src/Resources/public/js/main.js
+++ src/Resources/public/js/main.js
@@ -1,3 +1,50 @@
+// navbar
+const mobileMenu = document.querySelector(".nav__mobile__menu");
+const menuBody = document.querySelector(".header__nav__wrapper");
+if (mobileMenu) {
+  mobileMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    mobileMenu.classList.toggle("active-menu");
+    menuBody.classList.toggle("active-menu");
+  });
+}
+
+// Скрол к секции
+const menulinks = document.querySelectorAll(".header__nav-link[data-goto]");
+if (menulinks.length > 0) {
+  menulinks.forEach((menulink) => {
+    menulink.addEventListener("click", onMenuLinkClick);
+  });
+  function onMenuLinkClick(e) {
+    const menulink = e.target;
+    if (
+      menulink.dataset.goto &&
+      document.querySelector(menulink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menulink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        pageYOffset -
+        document.querySelector("header").offsetHeight;
+
+      if (mobileMenu.classList.contains("active-menu")) {
+        document.body.classList.remove("_lock");
+        mobileMenu.classList.remove("active-menu");
+        menuBody.classList.remove("active-menu");
+      }
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+        duration: 500, 
+        easing: "linear", 
+      });
+      e.preventDefault();
+    }
+  }
+}
+
+// Slider
 $('.slider__gallery').slick({
     infinite: true,
     slidesToShow: 4,
@@ -23,3 +70,28 @@ $('.slider__gallery').slick({
       },
     ],
   });
+
+  // MODAL
+
+let modal = document.querySelector(".modal");
+let moreDetailsButton = document.querySelectorAll(".nav__left, .nav__right");
+let closeBtn = document.querySelector(".btn-close");
+
+moreDetailsButton.forEach((item) => {
+  item.addEventListener("click", function () {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+  });
+});
+
+function closeModal() {
+  modal.classList.remove("show");
+  modal.classList.add("hide");
+}
+closeBtn.addEventListener("click", closeModal);
+
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
